2c3981617891e7c4b9566699c16c70055050d5e1
Rheno Julius Andreas <rhenojulius@gmail.com>
2025-05-27 21:44:29 +0700

    add: second scenario

--- src/test/features/filterHomePage.feature
+++ src/test/features/filterHomePage.feature
@@ -6,3 +6,7 @@ Feature: Memilih destinasi tujuan penerbangan
     And user klik Cari Penerbangan dengan xpath "//button[@class='bg-darkblue4 text-white p-2 rounded-lg']"
     Then user redirect ke halaman pilih penerbagnan "http://localhost:3000/users/public/detailPenerbangan?airportIdFrom=0196a5d9-347e-7ff3-83ad-c83e2f2e1342&airportIdTo=0196a5d9-3478-7e72-90f3-4058115ba17c&departureDate=2025-06-15"
 
+  Scenario: User belum login memilih penerbangan
+    Given user berada di halaman pilih penerbangan "http://localhost:3000/users/public/detailPenerbangan?airportIdFrom=0196a5d9-347e-7ff3-83ad-c83e2f2e1342&airportIdTo=0196a5d9-3478-7e72-90f3-4058115ba17c&departureDate=2025-06-15"
+    When user klik tombol pilih pada pilihan penerbangan dengan xpath "//button[normalize-space()='Pilih']"
+    Then user diarahkan ke halaman login "http://localhost:3000/login"
--- src/test/features/step_definitions/FilterHomePageSteps.js
+++ src/test/features/step_definitions/FilterHomePageSteps.js
@@ -1,10 +1,13 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const { Builder, until } = require("selenium-webdriver");
-const HomePage = require("../../pages/HomePage"); // sesuaikan dengan path kamu
+const HomePage = require("../../pages/HomePage"); 
 
 let driver;
 let homePage;
 
+// ----------------------------
+// SCENARIO 1: Halaman Utama
+// ----------------------------
 Given("user membuka halaman utama {string}", async function (url) {
   driver = await new Builder().forBrowser("chrome").build();
   homePage = new HomePage(driver);
@@ -27,6 +30,32 @@ When(
 
 Then(
   "user redirect ke halaman pilih penerbagnan {string}",
+  async function (expectedUrl) {
+    await driver.wait(async () => {
+      const currentUrl = await driver.getCurrentUrl();
+      return currentUrl === expectedUrl;
+    }, 10000);
+  }
+);
+
+// ------------------------------------------
+// SCENARIO 2: User belum login pilih tiket
+// ------------------------------------------
+Given("user berada di halaman pilih penerbangan {string}", async function (url) {
+  driver = await new Builder().forBrowser("chrome").build();
+  homePage = new HomePage(driver);
+  await homePage.open(url);
+});
+
+When(
+  "user klik tombol pilih pada pilihan penerbangan dengan xpath {string}",
+  async function (xpath) {
+    await homePage.klikByXpath(xpath);
+  }
+);
+
+Then(
+  "user diarahkan ke halaman login {string}",
   async function (expectedUrl) {
     await driver.wait(async () => {
       const currentUrl = await driver.getCurrentUrl();
