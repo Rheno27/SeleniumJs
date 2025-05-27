52f15dc9ec64f56803f5fa679e449ea3b81698a5
Rheno Julius Andreas <rhenojulius@gmail.com>
2025-05-27 22:22:43 +0700

    update setup projec and add scenario 3

--- src/test/features/Unauthenticated.feature
+++ src/test/features/Unauthenticated.feature
@@ -10,3 +10,10 @@ Feature: Memilih destinasi tujuan penerbangan
     Given user berada di halaman pilih penerbangan "http://localhost:3000/users/public/detailPenerbangan?airportIdFrom=0196a5d9-347e-7ff3-83ad-c83e2f2e1342&airportIdTo=0196a5d9-3478-7e72-90f3-4058115ba17c&departureDate=2025-06-15"
     When user klik tombol pilih pada pilihan penerbangan dengan xpath "//button[normalize-space()='Pilih']"
     Then user diarahkan ke halaman login "http://localhost:3000/login"
+
+  Scenario: User melakukan login
+    Given user membuka halaman utama "http://localhost:3000/login"
+    When user mengisi email "admin@gmail.com" dengan xpath "//input[@id='email']"
+    And user mengisi password "1234567890" dengan xpath "//input[@id='password']"
+    And user klik masuk untuk login dengan xpath "//button[normalize-space()='Masuk']"
+    Then user redirect ke halaman utama "http://localhost:3000/"
---
+++ src/test/features/step_definitions/UnauthenticatedSteps.js
@@ -0,0 +1,104 @@
+const { Given, When, Then } = require("@cucumber/cucumber");
+const { Builder, until } = require("selenium-webdriver");
+const HomePage = require("../../pages/UnauthenticatedPage");
+
+let driver;
+let homePage;
+
+// ----------------------------
+// SCENARIO 1: Halaman Utama
+// ----------------------------
+Given("user membuka halaman utama {string}", async function (url) {
+  driver = await new Builder().forBrowser("chrome").build();
+  homePage = new HomePage(driver);
+  await homePage.open(url);
+});
+
+When(
+  "user klik pilihan destinasi favorit dengan xpath {string}",
+  async function (xpath) {
+    await homePage.klikByXpath(xpath);
+  }
+);
+
+When(
+  "user klik Cari Penerbangan dengan xpath {string}",
+  async function (xpath) {
+    await homePage.klikByXpath(xpath);
+  }
+);
+
+Then(
+  "user redirect ke halaman pilih penerbagnan {string}",
+  async function (expectedUrl) {
+    await driver.wait(async () => {
+      const currentUrl = await driver.getCurrentUrl();
+      return currentUrl === expectedUrl;
+    }, 10000);
+  }
+);
+
+// ------------------------------------------
+// SCENARIO 2: User belum login pilih tiket
+// ------------------------------------------
+Given(
+  "user berada di halaman pilih penerbangan {string}",
+  async function (url) {
+    driver = await new Builder().forBrowser("chrome").build();
+    homePage = new HomePage(driver);
+    await homePage.open(url);
+  }
+);
+
+When(
+  "user klik tombol pilih pada pilihan penerbangan dengan xpath {string}",
+  async function (xpath) {
+    await homePage.klikByXpath(xpath);
+  }
+);
+
+Then("user diarahkan ke halaman login {string}", async function (expectedUrl) {
+  await driver.wait(async () => {
+    const currentUrl = await driver.getCurrentUrl();
+    return currentUrl === expectedUrl;
+  }, 10000);
+  await driver.quit();
+});
+
+// -------------------------------------
+// SCENARIO 3: User melakukan login
+// -------------------------------------
+Given("user membuka halaman login {string}", async function (url) {
+  driver = await new Builder().forBrowser("chrome").build();
+  homePage = new HomePage(driver);
+  await homePage.open(url);
+});
+
+When(
+  "user mengisi email {string} dengan xpath {string}",
+  async function (email, xpath) {
+    await homePage.isiInputByXpath(xpath, email);
+  }
+);
+
+When(
+  "user mengisi password {string} dengan xpath {string}",
+  async function (password, xpath) {
+    await homePage.isiInputByXpath(xpath, password);
+  }
+);
+
+When(
+  "user klik masuk untuk login dengan xpath {string}",
+  async function (xpath) {
+    await homePage.klikByXpath(xpath);
+  }
+);
+
+Then("user redirect ke halaman utama {string}", async function (expectedUrl) {
+  await driver.wait(async () => {
+    const currentUrl = await driver.getCurrentUrl();
+    return currentUrl === expectedUrl;
+  }, 10000);
+  await driver.quit();
+});
